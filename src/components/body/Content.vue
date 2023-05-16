b469350a3e9c010f3534894800a18e4a32cb519c
shyamporiya <shyamporiya9595@gmail.com>
2023-05-16 19:05:43 +0530

    add table

--- src/components/body/Content.vue
+++ src/components/body/Content.vue
@@ -1,97 +1,22 @@
 <template>
   <div>
     <h1>Welcome To Vue Js</h1>
-    <h3>Example of Increment Or Decrement</h3>
-    <hr />
-    <input
-      type="number"
-      class="value-text"
-      placeholder="Enter Value"
-      v-model="countVal"
-    />
-    <button class="btn btn-success" @click="addVal()">Add Value</button>
-    <br />
-    <button class="increment-btn btn btn-primary" @click="inc_btn()">
-      <i style="inc-btn">+</i>
-    </button>
-
-    <span type="number" class="showCount">{{ count }}</span>
-    <button class="decrement-btn btn btn-warning" @click="dect_btn()">
-      <i class="dec-btn">-</i>
-    </button>
-    <hr />
-    <h3>Example of v-model</h3>
-    <h2 aria-placeholder="v-model example">{{ model_Val }}</h2>
-    <input
-      class="model-val"
-      type="text"
-      placeholder="Enter value v-model"
-      v-model="model_Val"
-      data-title="Enter Value To Show Model"
-    />
+    <h3>Task</h3>
+    <p class="table_room"></p>
+    <p class="room-name">Room 1</p>
+    <p class="room-name">Adult</p>
+    <p class="room-name">Child</p>
   </div>
 </template>
-<script>
-export default {
-  name: "App",
-  component: {},
-  data() {
-    return {
-      count: "",
-      countVal: "",
-      model_Val: "",
-    };
-  },
-  methods: {
-    addVal() {
-      this.count = this.countVal;
-    },
-
-    inc_btn() {
-      console.log(" increment clicked");
-      this.count++;
-      if (this.count <= 0) {
-        console.log("only 0");
-      } else {
-        // this.example_values++;
-      }
-    },
-    dect_btn() {
-      console.log("decrement clicked");
-      if (this.count !== this.countVal && this.count !== 0) {
-        // /alert("Minimum Value Is 0");
-        //  console.log("worked");
-        this.count--;
-      } else if (this.count === this.countVal) {
-        alert("Your Value Is : " + this.countVal + " You Can't Decrement");
-      } else if (this.count === 0) {
-        alert("Minimum Value 0");
-      }
-    },
-  },
-};
-</script>
+<script></script>
 
 <style scoped>
-.increment-btn,
-.decrement-btn {
-  border: 2px solid rgb(7, 10, 0);
-  margin-left: 5px;
-  margin-right: 5px;
+.table_room {
+  height: 149px;
+  width: 300px;
+  border: 2px solid black;
+  margin-left: 473px;
 }
-.value-text {
-  margin-right: 25px;
-}
-.showCount {
-  padding: 2px 27px 8px 28px;
-  border: 3px solid black;
-  font-weight: 700;
-}
-i {
-  font-weight: bold;
-}
-hr {
-  border: 3px solid #e91818 !important;
+.room-name {
 }
 </style>
->
